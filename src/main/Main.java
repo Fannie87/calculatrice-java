a91d3dd10d166a656e2303505f227695184aac28
athebaut <a_thebaut@hotmail.fr>
2023-07-05 12:08:35 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,74 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); // System.in is a standard input stream
-		System.out.println("Enter a string: ");
-		String str = sc.nextLine(); // reads string
-		System.out.println("Enter a string1: ");
-		String str1 = sc.nextLine(); // reads string
-		System.out.println("Enter a string2: ");
-		String str2 = sc.nextLine(); // reads string
+  static List<String> operations = Arrays.asList("+", "-", "*", "/");
 
-		// recuperer 1er nombre verifier que c'est un nombre -> demander nouvelle entrée si pas un nombre
-		
-		// recuperer opération -> 4 opérations possibles + - * /
-		
-		
-		// recuperer 2e nombre
-		
-		// Afficher opération
-		
-		// Demander si le client veut une nouvelle opération
-		
-		System.out.println("You have entered: " + str);
-		System.out.println("You have entered: " + str1);
-		System.out.println("You have entered: " + str2);
-
-		
-	}
-
+  @SuppressWarnings("resource")
+  public static void main(String[] args) {
+    boolean continuerProgramme = true;
+    while(continuerProgramme) {
+      System.out.println("Nouvelle opération");
+      System.out.print("Entrer un chiffre : ");
+  
+      long chiffre1 = 0;
+      boolean testChiffre1 = true;
+      while (testChiffre1) {
+        try {
+          chiffre1 = new Scanner(System.in).nextLong();
+          testChiffre1 = false;
+        } catch (Exception e) {
+          System.out.print("Votre saisie est incorrecte. Veuillez entrer un nouveau chiffre : ");
+        }
+      }
+  
+      System.out.print("Entrer une opération [-,+,/,*] : ");
+      boolean testOperation = true;
+      String operation = "";
+      while (testOperation) {
+        operation = new Scanner(System.in).nextLine();
+        if (operations.contains(operation))
+          testOperation = false;
+        else
+          System.out.print("Votre saisie est incorrecte. Veuillez entrer un nouvelle opération [-,+,/,*] : ");
+      }
+  
+      System.out.print("Entrer un deuxième chiffre : ");
+      long chiffre2 = 0;
+      boolean testChiffre2 = true;
+      while (testChiffre2) {
+        try {
+          chiffre2 = new Scanner(System.in).nextLong();
+          if (chiffre2 == 0l && operation.equals("/"))
+            System.out.print("On ne peut pas diviser par zéro. Veuillez entrer un nouveau chiffre : ");
+          else
+            testChiffre2 = false;
+        } catch (Exception e) {
+          System.out.print("Votre saisie est incorrecte. Veuillez entrer un nouveau chiffre : ");
+        }
+      }
+  
+      double resultat = 0.0;
+      if (operation.equals("+"))
+        resultat = Math.addExact(chiffre1, chiffre2);
+      else if (operation.equals("-"))
+        resultat = Math.subtractExact(chiffre1, chiffre2);
+      else if (operation.equals("*"))
+        resultat = Math.multiplyExact(chiffre1, chiffre2);
+      else if (operation.equals("/"))
+        resultat = Math.floorDiv(chiffre1, chiffre2);
+  
+      System.out.println("Résultat: " + chiffre1 + " " + operation + " " + chiffre2 + " = " + resultat);
+      System.out.print("Voulez vous refaire une opération (O / N) ? ");
+      String nouvelleOperation = new Scanner(System.in).nextLine();
+      if(nouvelleOperation.equals("N")) {
+        continuerProgramme = false;
+        System.out.println("Fin du programme. Au revoir");
+        System.exit(0);
+      }
+    }
+  }
 }
